c290344a9ed4f318cd159736e201eb458e0ae6e1
Nguyen Ba Hung <hung@bio-rithm.com>
2023-08-22 11:22:19 +0700

    Add authors

--- cicd/test_authors.py
+++ cicd/test_authors.py
@@ -32,6 +32,8 @@ m_acceptAuthors = ['Amrish Nair <amrish@bio-rithm.com>',
 'Tran Minh Hai <hai@bio-rithm.com>',
 'Trishna Saeharaseelan <trishna@bio-rithm.com>',
 'Tom Li <tom.li@zuhlke.com>',
+'shahzad afridi <shahzadahmadafridi@gmail.com>',
+'Shahzad Afridi (Opriday) <shahzadahmadafridi@gmail.com>'
 ]
 
 m_acceptAuthorsDiffName = {

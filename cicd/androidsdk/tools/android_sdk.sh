734f1914912ac66c89a2999c9fa2861d058a2c7c
Hung Nguyen <hungnb94@gmail.com>
2023-08-23 05:00:26 +0700

    Install build-tools;30.0.3

--- cicd/androidsdk/tools/android_sdk.sh
+++ cicd/androidsdk/tools/android_sdk.sh
@@ -18,5 +18,6 @@ yes | sdkmanager --licenses
 echo "*** Install Android SDK Platform 33 ***"
 sdkmanager "platforms;android-33"
 sdkmanager "emulator"
+sdkmanager "build-tools;30.0.3"
 
 chmod -R 777 $ANDROID_HOME

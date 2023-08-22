8b1e7051ea359d9f27a53837468d6f588239aaf1
Hung Nguyen <hungnb94@gmail.com>
2023-08-22 15:55:28 +0700

    Remove unused PATH

--- cicd/androidsdk/tools/android_sdk.sh
+++ cicd/androidsdk/tools/android_sdk.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-mkdir -p /opt/android-sdk-linux/bin/
+source /opt/tools/android_env.sh
 
+mkdir -p $ANDROID_HOME
 cd $ANDROID_HOME
-echo "Set ANDROID_HOME to ${ANDROID_HOME}"
 
-echo "Copying Tools"
-mkdir -p ${ANDROID_HOME}/bin
-cp -v /opt/tools/*.sh ${ANDROID_HOME}/bin
-
-source /opt/android-sdk-linux/bin/android_env.sh
 
 echo "Bootstrapping SDK-Tools"
 wget -q https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O commandlinetools-linux.zip
@@ -19,17 +14,10 @@ mkdir cmdline-tools
 mv tools cmdline-tools/tools
 rm commandlinetools-linux.zip
 
-echo "Make sure repositories.cfg exists"
-mkdir -p ~/.android/
-touch ~/.android/repositories.cfg
-
-echo $PATH
-
 echo "*** Automatically accept all SDK licences ***"
 yes | sdkmanager --licenses
 
 echo "*** Install build tools 30.0.3 ***"
 sdkmanager "platforms;android-30"
-sdkmanager --list | grep ndk
 sdkmanager --install "ndk-bundle"
 sdkmanager --install "build-tools;30.0.3"

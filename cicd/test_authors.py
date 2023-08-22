bfd469d4ec2d0c9681ed0d813d25dfd84a8e7bfe
Nguyen Ba Hung <hung@bio-rithm.com>
2023-08-22 11:12:52 +0700

    Update test authors

--- cicd/test_authors.py
+++ cicd/test_authors.py
@@ -1,3 +1,18 @@
+################################################################################
+# Copyright ©2020-2021. Biorithm Pte Ltd. All Rights Reserved. # This software
+# is the property of Biorithm Pte Ltd. It must not be copied, printed,
+# distributed, or reproduced in whole or in part or otherwise disclosed without
+# prior written consent from Biorithm.This document may follow best coding
+# practices for Python as suggested in https://www.python.org/dev/peps/pep-0008/
+#
+# Filename: femom_test_authors.py
+# Original Author: PHAM DUY HOAN
+# Date created: 19 JAN 2021
+# Purpose: this module to test is there any wrong author in git commit
+# Revision History Raises (if any issues):
+
+################################################################################
+
 # ==============================IMPORT LIB======================================
 # ======================IMPORT PYTHON BUILT-IN LIB==============================
 import os
@@ -6,8 +21,17 @@ import subprocess
 
 # ==========================IMPORT USER LIB=====================================
 m_acceptAuthors = ['Amrish Nair <amrish@bio-rithm.com>',
+'Lavanya Baskaran <lavanya@bio-rithm.com>',
+'Li, Tom <tom.li@zuhlke.com>',
+'Luong Nhu Toan <toan@bio-rithm.com>',
 'Luong Thi Chuyen <chuyen@bio-rithm.com>',
 'Nguyen Ba Hung <hung@bio-rithm.com>',
+'Pham Duy Hoan <hoan@bio-rithm.com>',
+'Russell JVM Gutierrez <russell.gutierrez@zuhlke.com>',
+'Santhosh Kumar Gaikwad Ambaji Rao <santhosh@bio-rithm.com>',
+'Tran Minh Hai <hai@bio-rithm.com>',
+'Trishna Saeharaseelan <trishna@bio-rithm.com>',
+'Tom Li <tom.li@zuhlke.com>',
 ]
 
 m_acceptAuthorsDiffName = {

5084c1da8dfe1fbf1a39f8a189038c32966d9ad5
Hung Nguyen <hungnb94@gmail.com>
2023-08-23 15:00:56 +0700

    Download emulator for Mac Apple Silicon

--- cicd/androidsdk/tools/emulator.sh
+++ cicd/androidsdk/tools/emulator.sh
@@ -3,7 +3,7 @@
 cd $ANDROID_HOME
 #31.2.10
 echo "Download emulator manually"
-wget -q https://redirector.gvt1.com/edgedl/android/repository/emulator-linux_x64-8420304.zip -O emulator-linux.zip
+wget -q https://redirector.gvt1.com/edgedl/android/repository/emulator-darwin_aarch64-8420304.zip -O emulator-linux.zip
 unzip emulator-linux.zip
 rm emulator-linux.zip
 

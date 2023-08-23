b28543f9ebdd0b4d0110190d3a56475120731532
Hung Nguyen <hungnb94@gmail.com>
2023-08-23 17:51:54 +0700

    Install libc6-amd64-cross if needed

--- cicd/androidsdk/tools/commandline_tools.sh
+++ cicd/androidsdk/tools/commandline_tools.sh
@@ -15,20 +15,4 @@ rm commandlinetools-linux.zip
 echo "*** Automatically accept all SDK licences ***"
 yes | sdkmanager --licenses
 
-echo "*** Install Android SDK Platform 33 ***"
-sdkmanager --list | grep "build-tools"
-sdkmanager "platforms;android-33"
-
-echo "*** Install Android Emulator ***"
-if [ "$TARGETARCH" = "arm64" ]; then
-  echo "Install Android Emulator Manually because run on MacOS"
-  source /opt/tools/emulator.sh
-else
-  echo "Install Android Emulator Automatically"
-  sdkmanager "emulator"
-fi
-
-echo "*** Install Android SDK Build-Tools ***"
-sdkmanager "build-tools;30.0.3"
-
 chmod -R 777 $ANDROID_HOME
--- cicd/androidsdk/tools/emulator.sh
+++ cicd/androidsdk/tools/emulator.sh
@@ -2,10 +2,16 @@
 
 cd $ANDROID_HOME
 
-echo "Download emulator manually"
-# This is link download emulator for Mac Apple Silicon
-wget -q https://redirector.gvt1.com/edgedl/android/repository/emulator-darwin_aarch64-8420304.zip -O emulator-linux.zip
-unzip emulator-linux.zip
-rm emulator-linux.zip
+echo "*** Install Android Emulator ***"
+if [ "$TARGETARCH" = "arm64" ]; then
+  apt-get install -y libc6-amd64-cross
+  ln -s /usr/x86_64-linux-gnu/lib64/ /lib64
+  # This is link download emulator for Mac Apple Silicon
+  wget -q https://redirector.gvt1.com/edgedl/android/repository/emulator-darwin_aarch64-8420304.zip -O emulator-linux.zip
+  unzip emulator-linux.zip
+  rm emulator-linux.zip
+else
+  sdkmanager "emulator"
+fi
 
 mv /opt/license/package.xml $ANDROID_HOME/emulator

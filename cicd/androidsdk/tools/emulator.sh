9809875de808ccd843eb4fac7749297e41a008d4
Hung Nguyen <hungnb94@gmail.com>
2023-08-25 16:40:01 +0700

    Install libgcc1

--- cicd/androidsdk/tools/emulator.sh
+++ cicd/androidsdk/tools/emulator.sh
@@ -4,7 +4,7 @@ cd $ANDROID_HOME
 
 echo "*** Install Android Emulator ***"
 if [ "$TARGETARCH" = "arm64" ]; then
-  apt-get install -y libc6-amd64-cross
+  apt-get install -y libc6-amd64-cross libgcc1-amd64-cross
   ln -s /usr/x86_64-linux-gnu/lib64/ /lib64
   # This is link download emulator for Mac Apple Silicon
   wget -q https://redirector.gvt1.com/edgedl/android/repository/emulator-darwin_aarch64-8420304.zip -O emulator-linux.zip
